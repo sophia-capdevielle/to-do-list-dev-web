e6a939f133ddf104041235ed4d59f74ee06b83e0
sophia-capdevielle <sophia.capdevielle2@gmail.com>
2024-11-15 15:59:29 +0100

    Upload all

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Screenshot To do list
+
+![visuel](./image.png)
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from 'react';
 
-function App() {
+function Tache(task) {
+  const { id, title, completed } = task;
+  const [taskCompleted, setTaskCompleted] = useState(completed)
+
+  const toggleCompletion = () => {
+    fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ done: !completed }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          setTaskCompleted(!taskCompleted)
+        } else {
+          console.error("Failed to update the task");
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="checkbox"
+        checked={taskCompleted}
+        onChange={toggleCompletion}
+      />
+      <span className="tache-text">{title}</span>
     </div>
   );
 }
 
-export default App;
+
+
+function TacheTable({filterCompleted}){
+
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState(''); 
+
+  useEffect(() => {
+    fetch("http://localhost:5000/tasks")
+      .then(response => response.json())
+      .then(data => setTasks(data));
+  }, []);
+
+  const addTask = (text) => {
+    fetch('http://localhost:5000/tasks/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title: text }),
+    })
+    .then(response => response.json())
+    .then(newTask => {
+      setTasks(prevTasks => [newTask, ...prevTasks]);
+    });
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim()) {
+      addTask(inputValue);
+      setInputValue(''); 
+    }
+  };
+
+  return(
+    <div>
+    {tasks.map((task, index) => {
+      if (filterCompleted && task.done){
+        return null;
+      }
+        return (
+          <Tache
+            key={task.id}
+            title={task.title}
+            index={index}
+            completed={task.done}
+            id={task.id}
+          />
+        );
+      })}
+
+    <form className="add-tache" onSubmit={handleSubmit}>
+      <button type="submit" className="add-button"> + </button>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Titre de la tâche"
+        className="add-input"
+      />
+    </form>
+    </div>
+  )
+}
+
+function Filter({ filterCompleted, toggleFilter }){
+  return(
+    <button className="filter-button" onClick={toggleFilter}>
+    {filterCompleted ? 'Afficher toutes les tâches' : 'Masquer les tâches réalisées'}
+    </button>
+  )
+}
+
+export default function TodoTable(){
+  const [filterCompleted, setFilterCompleted] = useState(false);
+
+  const toggleFilter = () => {
+    setFilterCompleted(!filterCompleted);
+  };
+
+  return(
+    <div className="todo-table">
+      <h1 className="todo-title">TO DO</h1>
+      <TacheTable filterCompleted={filterCompleted} />
+      <Filter filterCompleted = {filterCompleted} toggleFilter = {toggleFilter}/>
+    </div>
+  )
+}
